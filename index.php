a0e992d262bf762192464899992f942589f84bb1
manuelnones <manuelnones99@yahoo.com>
2023-05-04 17:50:13 +0200

    add extension class productGame

--- index.php
+++ index.php
@@ -9,18 +9,31 @@ class product {
         $this->brand = $brand;
         $this->price = $price;
     }
-
 }
 
 class productFood extends product {
-    public $taste;
     public $weight;
+    public $taste;
 
     function __construct(string $brand, string $weight, string $taste, int $price,) {
         parent::__construct($brand, $price);
         
         $this->weight = $weight;
         $this->taste = $taste;
+    }
+}
+
+class productGame extends product {
+    public $nameGame;
+    public $color;
+    public $size;
+
+    function __construct(string $nameGame, string $brand, string $color, string $size, int $price,) {
+        parent::__construct($brand, $price);
+        
+        $this->nameGame = $nameGame;
+        $this->color = $color;
+        $this->size = $size;
     }
 }
 
@@ -36,5 +49,10 @@ $dogsFood3 = new productFood("N&D", "2.5kg", "agnello e mirtillo", 28);
 $dogsFood4 = new productFood("Dagel dogs", "20kg", "manzo e agnello", 37);
 
 
-var_dump($catsFood5);
-var_dump($dogsFood1);
+$catGame1 = new productGame("Canna da pesca", "Gim cat", "giallo e rosso", "48 cm", 4);
+$catGame2 = new productGame("Pira-topino", "Camon", "grigio", "7 cm", 3);
+
+$dogGame1 = new productGame("Kong Air Squeaker Tennis Ball", "Kong", "verde", "large", 2); 
+$dogGame2 = new productGame("Gioco osso interattivo", "Gim dog", "arancione", "17,5 cm", 6);
+
+var_dump($dogGame2);
